33ae539c58ab6828d96ebfccfed5d3e7b645923f
Joseph Billy <ingbillyjoseph@yahoo.com>
2021-10-02 12:46:57 -0400

    crete edit page

---
+++ components/layouts/profileNav.js
@@ -0,0 +1,23 @@
+import Link from "next/link"
+
+export default function ProfileNav() {
+  return (
+
+    <div className="col-lg-6 col-md-6 offset-lg-1">
+    <div className="profile-menu-wrapper">
+      <div className="main-menu-inner header-top-navigation">
+        <nav>
+          <ul className="main-menu">
+            <li className="active"><a href="#">timeline</a></li>
+            <li><a href="#">about</a></li>
+            <li><a href="#">Bet history</a></li>
+            <li><a href="#">Payment History</a></li>
+            <li className="d-inline-block d-md-none"><a href="editprofile">edit profile</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </div>
+  
+    )
+}

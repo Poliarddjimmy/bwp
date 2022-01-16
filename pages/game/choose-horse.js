3b825a1ac5601ec0dead3cb5012f043267617f5a
Joseph Andy Feidje <ingandy75@gmail.com>
2022-01-15 21:11:38 -0500

    horses data fetched from backend

--- pages/game/choose-horse.js
+++ pages/game/choose-horse.js
@@ -37,55 +37,55 @@ const Choosehorse = ({ currentUser }) => {
         <div className="container">
 
           <div className="row">
-
-            <div className="flip-card col-lg-4 col-md-4 col-sm-4 col-12">
-              <Link href="/game/horse-details" passHref>
-                <div className="flip-card-inner single-bonus border-danger p-2 cursor-pointer">
-                  <div className="flip-card-front d-flex flex-column justify-content-center text-left p-2">
-                    <Image className="img-flip" src="/images/horses/horse_1_b.png" alt="" width="150" height="120" />
-                    <strong>Rambo 1</strong>
-                    <div className="text color-one">
-                      <small>Speed 1.3 Km Per Hour</small>
+            {horses.map(horse => (
+              <div className="flip-card col-lg-4 col-md-4 col-sm-4 col-12" key={horse.id}>
+                <Link href="/game/horse-details" passHref>
+                  <div className="flip-card-inner single-bonus border-danger p-2 cursor-pointer">
+                    <div className="flip-card-front d-flex flex-column justify-content-center text-left p-2">
+                      <Image className="img-flip" src={`/images/horses/horse_${horse.number}_b.png`} alt="" width="150" height="120" />
+                      <strong>{horse.name}</strong>
+                      <div className="text color-one">
+                        <small>{horse.description}</small>
+                      </div>
+                    </div>
+                    <div className="flip-card-back text-left p-3">
+                      <div className="d-flex align-items-center justify-content-center w-100">
+                        <Image src={`/images/horses/horse_${horse.number}_b.png`} alt="" width="150" height="80" />
+                      </div>
+                      <strong>{horse.name}</strong>
+                      <div className="text color-one">
+                        <small>{horse.description}</small>
+                      </div>
+                      <hr className="border-danger-hr m-1" />
+                      <strong>Details</strong>
+                      <div className="pl-4">
+                        <ul>
+                          <li><small>Wining Sreck: 10</small></li>
+                          <li><small>Color: Lid Brown</small></li>
+                          <li><small>Eye Color: Light Brown</small></li>
+                          <li><small>Star:Ethicatcum</small></li>
+                        </ul>
+                      </div>
+                      <hr className="border-danger-hr m-1" />
+                      <div className="rating d-flex">
+                        <strong className="mr-3">Ratings:</strong>
+                        {[...Array(4)].map((star, i) => {
+                          const ratingValue = i + 1;
+                          return (
+                            <label key={i}>
+                              <span className="icon text-warning">★</span>
+                            </label>
+                          )
+                        })}
+                        <label>
+                          <span className="icon">★</span>
+                        </label>
+                      </div>
                     </div>
                   </div>
-                  <div className="flip-card-back text-left p-3">
-                    <div className="d-flex align-items-center justify-content-center w-100">
-                      <Image src="/images/horses/horse_1_b.png" alt="" width="150" height="80" />
-                    </div>
-                    <strong>Rambo 1</strong>
-                    <div className="text color-one">
-                      <small>Speed 1.3 Km Per Hour</small>
-                    </div>
-                    <hr className="border-danger-hr m-1" />
-                    <strong>Details</strong>
-                    <div className="pl-4">
-                      <ul>
-                        <li><small>Wining Sreck: 10</small></li>
-                        <li><small>Color: Lid Brown</small></li>
-                        <li><small>Eye Color: Light Brown</small></li>
-                        <li><small>Star:Ethicatcum</small></li>
-                      </ul>
-                    </div>
-                    <hr className="border-danger-hr m-1" />
-                    <div className="rating d-flex">
-                      <strong className="mr-3">Ratings:</strong>
-                      {[...Array(4)].map((star, i) => {
-                        const ratingValue = i + 1;
-                        return (
-                          <label key={i}>
-                            <span className="icon text-warning">★</span>
-                          </label>
-                        )
-                      })}
-                      <label>
-                        <span className="icon">★</span>
-                      </label>
-                    </div>
-                  </div>
-                </div>
-              </Link>
-
-            </div>
+                </Link>
+              </div>
+            ))}
           </div>
         </div>
       </section>
